2a01a6758f1ee7a6bbcb536c338d8d0bedf23e08
Brandon Cardoso <cardosobrandon@gmail.com>
2018-06-05 19:17:26 -0400

    add mariadb, views dir

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
+var Client = require('mariasql')
 var express = require('express')
 var app = express()
 
+var client = new Client({
+    host: '127.0.0.1',
+    user: 'root',
+    password: ''
+})
+
 app.get('/', function (req, res) {
-    res.send('hello world')
+    //res.sendFile('home.html', {'root': __dirname + '/views'})
+    client.query('SHOW DATABASES', function (err, rows) {
+        if (err) {
+            throw err
+        }
+    })
 })
 
 app.listen(3000, function() {
