423e3eb5259b151f2159ffae857d0397e7acc4e8
Brandon Cardoso <cardosobrandon@gmail.com>
2018-06-08 21:16:04 -0400

    setup bulma for overrides, basic home layout with nav

--- app/app.js
+++ app/app.js
@@ -1,11 +1,18 @@
+const sassMiddleware = require('node-sass-middleware')
+const path = require('path')
 const Sequelize = require('sequelize')
 const express = require('express')
-const path = require('path')
+
 const app = express()
 
 const config = require('./config')
 const dbs = require('./dbs')
 const routes = require('./routes')
+
+app.use(sassMiddleware({
+    src: 'public',
+    debug: true
+}))
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
