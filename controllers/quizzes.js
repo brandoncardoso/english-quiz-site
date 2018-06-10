dfaffc7cdde6d43021865f9bcc440edefe32a7c9
Brandon Cardoso <cardosobrandon@gmail.com>
2018-06-10 00:32:32 -0400

    structure quizlist, create fillintheblank question page

--- controllers/quizzes.js
+++ controllers/quizzes.js
@@ -1,3 +1,36 @@
+const _ = require('lodash')
+const qs = require('querystring')
+
+const numColumns = 3
+
+let quizList = [
+    {
+        name: 'The, A, & An',
+        particles: ['the', 'a', 'an']
+    },
+    {
+        name: 'In, At, &, On',
+        particles: ['in', 'at', 'on']
+    },
+    {
+        name: 'And, & But',
+        particles: ['and', 'but']
+    }
+]
+quizList = _.map(quizList, function(quiz) {
+    quiz.params = qs.stringify({ p: quiz.particles })
+    return quiz
+})
+quizList = _.chunk(quizList, _.ceil(quizList.length / numColumns))
+
 exports.index = function (req, res) {
-    res.render('quizzes', { quizList: ['The, A, & An', 'In, At, &, On', 'And, & But'] })
+    res.render('quizzes', {
+        quizList: quizList
+    })
+}
+
+exports.fillintheblank = function (req, res) {
+    res.render('fillintheblank', {
+        particles: _.get(req, 'query.p')
+    })
 }
--- routes/quizzes.js
+++ routes/quizzes.js
@@ -4,5 +4,6 @@ const router = express.Router()
 const quizzesController = require('../controllers/quizzes')
 
 router.get('/', quizzesController.index)
+router.get('/fillintheblank/:sentenceId', quizzesController.fillintheblank)
 
 module.exports = router
