eba601f69266d25366170c6bbe3018e01f28db9f
Brandon Cardoso <cardosobrandon@gmail.com>
2018-06-22 22:22:31 -0400

    use mysql session store

--- app/app.js
+++ app/app.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser")
 const Sequelize = require('sequelize')
 const express = require('express')
 const session = require('express-session')
+const MySQLStore = require('express-mysql-session')(session)
+const mysql = require('mysql2')
 
 const middleware = require('./middleware')
 
@@ -25,10 +27,23 @@ app.use(sassMiddleware({
 app.set('views', 'public/views')
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+
+const sessionStoreOptions = {
+    host: 'localhost',
+    port: '3306',
+    user: 'sessionstore',
+    password: 'poop123',
+    database: 'session'
+}
+
+const connection = mysql.createConnection(sessionStoreOptions)
+const sessionStore = new MySQLStore(sessionStoreOptions)
+
 app.use(session({
     secret: 'mwSlzMW7K9uQdQgQrUg1vqkN0W6bAPGMs4kyDpogndGjmIGbB6',
     resave: false,
-    saveUninitialized: true
+    saveUninitialized: true,
+    store: sessionStore
 }))
 app.use(middleware.user)
 
